c4a530e8b0f3cec8244097976dd1ab05811161ac
Himanshu <gghimanshu333@gmail.com>
2025-04-24 16:07:31 +0530

    version0.1 is done!! hurray!!!

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import webpack from 'webpack' 
+import webpack, { DefinePlugin } from 'webpack' 
 import CopyWebpackPlugin from 'copy-webpack-plugin'
 
 
@@ -49,6 +49,15 @@ const config: webpack.Configuration = {
         new CopyWebpackPlugin({
             patterns: [{from : 'static'}],
         }),
+        new DefinePlugin({
+            "process.env.API_KEY" : JSON.stringify('AIzaSyBU1pOlg1RXIGqYbE1_uMWwAa_9WB3Gcpw'),
+            "process.env.FIREBASE_API_KEY" : JSON.stringify("AIzaSyDD_n0qhvKuorl6YwJyIy9ObgfcUX3x6bE"),
+            "process.env.FIREBASE_AUTH_DOMAIN" : JSON.stringify("fluentify-7a78a.firebaseapp.com"),
+            "process.env.FIREBASE_PROJECT_ID" : JSON.stringify("fluentify-7a78a"),
+            "process.env.FIREBASE_STORAGE_BUCKET" : JSON.stringify("fluentify-7a78a.firebasestorage.app"),
+            "process.env.FIREBASE_SENDER_ID" : JSON.stringify("1096214608573"),
+            "process.env.FIREBASE_APP_ID" : JSON.stringify("1:1096214608573:web:e947b22b3542da8ba695d4")
+        })
     ]
 }
 
